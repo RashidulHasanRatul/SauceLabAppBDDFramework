c8353442dcdea2af919347c9c5c3546dfc0ed8b1
Rashidul Hasan <rhratul12@gmail.com>
2022-05-23 03:05:43 +0600

    Add runner File

--- src/test/java/com/qa/runners/MyRunnerTest.java
+++ src/test/java/com/qa/runners/MyRunnerTest.java
@@ -1,6 +1,23 @@
 package com.qa.runners;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/com/qa/features",
+        glue = "com/qa/step_definitions",
+        plugin = {"pretty", "html:target/cucumber-reports","summary"},
+        snippets=CAMELCASE,
+        dryRun = true,
+        monochrome = true
+        //tags = "@smoke"
+
+)
 
 public class MyRunnerTest {
+
 
 }

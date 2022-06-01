72e701b34e5487ca3beb736e62e043dc406571d5
Rashidul Hasan <rhratul12@gmail.com>
2022-06-02 01:43:26 +0600

    Driver initialized in before and after hook

--- src/test/java/com/qa/stepdef/Hooks.java
+++ src/test/java/com/qa/stepdef/Hooks.java
@@ -1,5 +1,6 @@
 package com.qa.stepdef;
 
+import com.qa.utils.DriverManager;
 import com.qa.utils.GlobalParams;
 import com.qa.utils.ServerManager;
 import io.cucumber.java.After;
@@ -7,18 +8,26 @@ import io.cucumber.java.Before;
 
 public class Hooks {
     @Before
-    public void initialize() {
+    public void initialize() throws Exception {
         GlobalParams params = new GlobalParams();
         params.initializeGlobalParams();
         new ServerManager().startServer();
+        new DriverManager().initializeDriver();
     }
 
     @After
-    public void quit() {
+    public void quit() throws Exception {
+        DriverManager driverManager = new DriverManager();
+        if (driverManager.getDriver() != null) {
+            driverManager.getDriver().quit();
+            driverManager.setDriver(null);
+        }
+
         ServerManager serverManager = new ServerManager();
-        if (serverManager.getServer()!=null) {
+        if (serverManager.getServer() != null) {
             serverManager.getServer().stop();
         }
+
 
     }
 }
